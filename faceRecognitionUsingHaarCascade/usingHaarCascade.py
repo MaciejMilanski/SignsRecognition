354d1d45e9b3e8b7867266773c007a089105bb66
Maciej <maciej.milanski@gmail.com>
2019-09-26 20:45:14 +0200

    new xml files for classifier

--- faceRecognitionUsingHaarCascade/usingHaarCascade.py
+++ faceRecognitionUsingHaarCascade/usingHaarCascade.py
@@ -14,26 +14,26 @@ while(1):
 
     _, THRESHimg1 = cv.threshold(GRAYimg1, 85, 255, cv.THRESH_BINARY)
 
-    face_cascade_front = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-    faces_front = face_cascade_front.detectMultiScale(THRESHimg1, 1.3, 5)
-    face_cascade_profile = cv.CascadeClassifier('haarcascade_profileface.xml')
-    faces_profile = face_cascade_profile.detectMultiScale(THRESHimg1, 1.3, 5)
+    A7 = cv.CascadeClassifier('A7stage5.xml')
+    A7Front = A7.detectMultiScale(GRAYimg1, 1.3, 5)
 
 
 
-    for (x, y, w, h) in faces_front:
+
+    for (x, y, w, h) in A7Front:
         cv.rectangle(RGBimg1, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = THRESHimg1[y:y + h, x:x + w]
         roi_color = RGBimg1[y:y + h, x:x + w]
-    for (x, y, w, h) in faces_profile:
+    for (x, y, w, h) in A7Front:
         cv.rectangle(RGBimg1, (x, y), (x + w, y + h), (0, 0, 255), 10)
         roi_gray = THRESHimg1[y:y + h, x:x + w]
         roi_color = RGBimg1[y:y + h, x:x + w]
-    cv.imshow('faceRecognition', RGBimg1)
+    cv.imshow('A7RecognitionRGB', RGBimg1)
+    cv.imshow('A7RecognitionGREY', GRAYimg1)
+    cv.imshow('A7RecognitionTHRESH', THRESHimg1)
 
     k = cv.waitKey(5) & 0xFF
     if k == 27:
         break
 
 cv.destroyAllWindows()
-# find better threshold for thresholding and find out if it better way at all
