91800b3398d612d814da60a49d8cc048e677a5cc
Maciej <maciej.milanski@gmail.com>
2019-10-15 11:23:43 +0200

    -

--- faceRecognitionUsingHaarCascade/usingHaarCascade.py
+++ faceRecognitionUsingHaarCascade/usingHaarCascade.py
@@ -2,36 +2,21 @@ import cv2 as cv
 import numpy as np
 
 cap = cv.VideoCapture(0)
-
+speed_limit_50_cascade = cv.CascadeClassifier('speed_limit_50_stage9_cascade.xml')
 while(1):
-
-    # Take each frame
     _, RGBimg1 = cap.read()
+    #cv.imshow('A7RecognitionRGB', RGBimg1)
+    # Take each frame
 
 
     # Convert BGR to GRAY
     GRAYimg1 = cv.cvtColor(RGBimg1,cv.COLOR_BGR2GRAY)
+    speed_limit_50 = speed_limit_50_cascade.detectMultiScale(GRAYimg1, 1.3, 5)
 
-    _, THRESHimg1 = cv.threshold(GRAYimg1, 85, 255, cv.THRESH_BINARY)
-
-    A7 = cv.CascadeClassifier('A7stage2.xml')
-    A7Front = A7.detectMultiScale(GRAYimg1, 1.3, 5)
-
-
-
-
-    for (x, y, w, h) in A7Front:
-        cv.rectangle(RGBimg1, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_gray = THRESHimg1[y:y + h, x:x + w]
-        roi_color = RGBimg1[y:y + h, x:x + w]
-    for (x, y, w, h) in A7Front:
+    for (x, y, w, h) in speed_limit_50:
         cv.rectangle(RGBimg1, (x, y), (x + w, y + h), (0, 0, 255), 10)
-        roi_gray = THRESHimg1[y:y + h, x:x + w]
         roi_color = RGBimg1[y:y + h, x:x + w]
     cv.imshow('A7RecognitionRGB', RGBimg1)
-    cv.imshow('A7RecognitionGREY', GRAYimg1)
-    cv.imshow('A7RecognitionTHRESH', THRESHimg1)
-
     k = cv.waitKey(5) & 0xFF
     if k == 27:
         break
