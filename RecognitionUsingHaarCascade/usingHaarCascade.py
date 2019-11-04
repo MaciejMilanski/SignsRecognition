b8090914bc5d2418107e30cd4cc43704b3df3561
Maciej <maciej.milanski@gmail.com>
2019-11-04 09:42:08 +0100

    -

--- RecognitionUsingHaarCascade/usingHaarCascade.py
+++ RecognitionUsingHaarCascade/usingHaarCascade.py
@@ -21,17 +21,38 @@ def filter(image):
     lowBlack = np.array([0, 0, 0])
     highBlack = np.array([255, 255, 120])
     mask = cv.inRange(HSVimage, lowBlack, highBlack)
-    mask = cv.bitwise_not(mask)
+    mask = cv.resize(mask,(200,200))
+    mask = mask[50: 150, 50: 150]
+    mask = cv.resize(mask,(50,50))
+    #mask = cv.bitwise_not(mask)
     cv.imshow('mask', mask)
     return mask
 
 def putOnTable(image):
-    table = cv.imread("table.jpg",1)
+    table = cv.imread("table.jpg",0)
     xOffset = 10
     yOffset = 10
     table[yOffset:yOffset + image.shape[0], xOffset:xOffset + image.shape[1]] = image
     table = cv.bitwise_not(table)
+    cv.imshow('table',table)
     cv.imwrite("text.jpg",table)
+
+def readText():
+    textImg = Image.open("text.jpg")
+    text = pytesseract.image_to_string(textImg, lang='eng')
+    return text
+
+def checkIftextIsNumber(text):
+    try:
+        int(text)
+    except ValueError:
+        return False
+
+def checkIfTextPossibyIsSpeedLimit(text):
+    if text % 5 == 0:
+        return True
+    else:
+        return False
 
 cap = cv.VideoCapture(0)
 
@@ -48,7 +69,7 @@ while(1):
     # Take each frame
     _, BGRimg1 = cap.read()
     #BGRimg1 = cv.imread('image.jpg', 1)
-    cv.imshow('RecognitionRGB', BGRimg1)
+    #cv.imshow('RecognitionRGB', BGRimg1)
     BGRimg1 = cv.resize(BGRimg1,(800,640))
 
     # Convert BGR to GRAY
@@ -62,8 +83,10 @@ while(1):
         cropSignImg = cropSign(speed_limit_10, BGRimg1, cropX, cropY)
         mask = filter(cropSignImg)
         putOnTable(mask)
-        textImg = Image.open("text.jpg")
-        text = pytesseract.image_to_string(textImg, lang='eng')
+        text = readText()
+        numberFlag = checkIftextIsNumber(text)
+        #speedLimitFlag = checkIfTextPossibyIsSpeedLimit(int(text))
+        #if numberFlag == True:# and speedLimitFlag == True:
         print(text)
 
     cv.imshow('cropGRAYimg1', cropGRAYimg1)
@@ -71,5 +94,5 @@ while(1):
     k = cv.waitKey(5) & 0xFF
     if k == 27:
         break
-    cv.waitKey(9999)
+    #cv.waitKey(9999)
 cv.destroyAllWindows()
