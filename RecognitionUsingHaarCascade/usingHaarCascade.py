94da7c3b48b2c102e4ce8411dedecb90681a74a4
Maciej <maciej.milanski@gmail.com>
2019-11-30 20:18:55 +0100

    -

--- RecognitionUsingHaarCascade/usingHaarCascade.py
+++ RecognitionUsingHaarCascade/usingHaarCascade.py
@@ -7,67 +7,93 @@ def printRectangle(sign, image, index, offsetx, offsety): # offsets are needed t
         cv.rectangle(image, (x + offsetx, y + offsety), (x + offsetx + w, y + offsety + h), (0, 0, 255), 10)
         roi_color = image[y:y + h, x:x + w]
         print(index)
+def cropSign(sign, image, offsetx, offsety):
+    for (x, y, w, h) in sign:
+        croppedSign = image[y + offsety : y + offsety + h, x + offsetx : x + offsetx + w]
+        cv.imshow('cropped text', croppedSign)
+        return croppedSign
 
+def putOnTable(image):
+    table = cv.imread("table.jpg",0)
+    xOffset = 100
+    yOffset = 100
+    table[yOffset:yOffset + image.shape[0], xOffset:xOffset + image.shape[1]] = image
+    #table = cv.bitwise_not(table)
+    cv.imshow('table',table)
+    cv.imwrite("text.jpg",table)
+    return table
 
+def filter(image):
+    HSVimage = cv.cvtColor(image,cv.COLOR_BGR2HSV)
+    lowBlack = np.array([200, 150, 0])
+    highBlack = np.array([255, 255, 97])
+    mask = cv.inRange(HSVimage, lowBlack, highBlack)
+    #mask = cv.resize(mask,(200,200))
+    #mask = mask[50: 150, 50: 150]
+    #mask = cv.resize(mask,(50,50))
+    #mask = cv.bitwise_not(mask)
+    cv.imshow('mask', mask)
+    return mask
 
+cap = cv.VideoCapture('MOVI4128.avi')
+#cap = cv.VideoCapture(0)
 
-cap = cv.VideoCapture(0)
-#warning signs
-A7_cascade = cv.CascadeClassifier('warning_signs_cascades/A7_cascade.xml')
-#information signs
-D2_cascade = cv.CascadeClassifier('information_signs_cascades/D2_cascade.xml')
 #speed limits
 speed_limit_5_cascade = cv.CascadeClassifier('speed_limit_cascades/speed_limit_5_cascade.xml')
 speed_limit_10_cascade = cv.CascadeClassifier('speed_limit_cascades/speed_limit_10_cascade.xml')
 speed_limit_20_cascade = cv.CascadeClassifier('speed_limit_cascades/speed_limit_20_cascade.xml')
 speed_limit_30_cascade = cv.CascadeClassifier('speed_limit_cascades/speed_limit_30_cascade.xml')
 speed_limit_40_cascade = cv.CascadeClassifier('speed_limit_cascades/speed_limit_40_cascade.xml')
-
+speed_limit_50_cascade = cv.CascadeClassifier('speed_limit_cascades/speed_limit_50_cascade.xml')
+speed_limit_60_cascade = cv.CascadeClassifier('speed_limit_cascades/speed_limit_60_cascade.xml')
 #coordinates or crop fcn
-cropX = 500
-cropY = 100
-
+cropX = 250
+cropY = 250
+#min and max Size of detected objects
+minSizeXY = 30
+maxSizeXY = 70
 while(1):
 
 
     # Take each frame
     _, BGRimg1 = cap.read()
+    #BGRimg1 = cv.imread('image5.jpg',1)
     BGRimg1 = cv.resize(BGRimg1,(800,640))
     # Convert BGR to GRAY
     GRAYimg1 = cv.cvtColor(BGRimg1,cv.COLOR_BGR2GRAY)
     #crop image
-    cropGRAYimg1 = GRAYimg1[cropY:cropY + 300 , cropX:cropX + 300]
-
-    #warning_signs
-    A7 = A7_cascade.detectMultiScale(cropGRAYimg1, 1.3, 5)
-
-    #information signs
-    D2 = D2_cascade.detectMultiScale(cropGRAYimg1, 1.3, 5)
+    #ret,THRESHimg1 = cv.threshold(GRAYimg1,125,130,cv.THRESH_BINARY)
+    #cv.imshow('fdfdf',THRESHimg1)
+    cropGRAYimg1 = GRAYimg1[cropY:cropY + 150 , cropX:cropX + 400]
+    #cropGRAYimg1 = cv.resize(cropGRAYimg1,(,1000))
 
     #speed limits
-    speed_limit_5 = speed_limit_5_cascade.detectMultiScale(cropGRAYimg1, 1.3, 5)
-    speed_limit_10 = speed_limit_10_cascade.detectMultiScale(cropGRAYimg1, 1.3, 5)
-    speed_limit_20 = speed_limit_20_cascade.detectMultiScale(cropGRAYimg1, 1.3, 5)
-    speed_limit_30 = speed_limit_30_cascade.detectMultiScale(cropGRAYimg1, 1.3, 5)
-    speed_limit_40 = speed_limit_40_cascade.detectMultiScale(cropGRAYimg1, 1.3, 5)
+    speed_limit_5 = speed_limit_5_cascade.detectMultiScale(cropGRAYimg1, 1.1, 15, 0, (minSizeXY, minSizeXY),(maxSizeXY,maxSizeXY))
+    speed_limit_10 = speed_limit_10_cascade.detectMultiScale(cropGRAYimg1, 1.1, 15, 0, (minSizeXY, minSizeXY),(maxSizeXY,maxSizeXY))
+    speed_limit_20 = speed_limit_20_cascade.detectMultiScale(cropGRAYimg1, 1.1, 15, 0, (minSizeXY, minSizeXY),(maxSizeXY,maxSizeXY))
+    speed_limit_30 = speed_limit_30_cascade.detectMultiScale(cropGRAYimg1, 1.1, 15, 0, (minSizeXY, minSizeXY),(maxSizeXY,maxSizeXY))
+    speed_limit_40 = speed_limit_40_cascade.detectMultiScale(cropGRAYimg1, 1.1, 15, 0, (minSizeXY, minSizeXY),(maxSizeXY,maxSizeXY))
+    speed_limit_50 = speed_limit_50_cascade.detectMultiScale(cropGRAYimg1, 1.20, 15, 0, (minSizeXY, minSizeXY),(maxSizeXY,maxSizeXY))
+    speed_limit_60 = speed_limit_60_cascade.detectMultiScale(cropGRAYimg1, 1.2, 11, 0, (minSizeXY, minSizeXY),(maxSizeXY, maxSizeXY))
 
-    #warning signs
-    printRectangle(A7, BGRimg1, "A7", cropX, cropY)
-
-    #information signs
-    printRectangle(D2, BGRimg1, "D2", cropX, cropY)
+    #crop_sign = cropSign(speed_limit_10, GRAYimg1, cropX, cropY)
+    # text = putOnTable(crop_sign)
+    # speed_limit_30 = speed_limit_30_cascade.detectMultiScale(text, 1.3, 8)
+    #filter(cropGRAYimg1)
 
     #speed limits
-    printRectangle(speed_limit_5, BGRimg1, 5, cropX, cropY)
-    printRectangle(speed_limit_10, BGRimg1, 10, cropX, cropY)
-    printRectangle(speed_limit_20, BGRimg1, 20, cropX, cropY)
-    printRectangle(speed_limit_30, BGRimg1, 30, cropX, cropY)
-    printRectangle(speed_limit_40, BGRimg1, 40, cropX, cropY)
+    #printRectangle(speed_limit_5, BGRimg1, 5, cropX, cropY)
+    #printRectangle(speed_limit_10, BGRimg1, 10, cropX, cropY)
+    #printRectangle(speed_limit_20, BGRimg1, 20, cropX, cropY)
+    #printRectangle(speed_limit_30, BGRimg1, 30, cropX, cropY)
+    #printRectangle(speed_limit_40, BGRimg1, 40, cropX, cropY)
+    printRectangle(speed_limit_50, BGRimg1, 50, cropX, cropY)
+    printRectangle(speed_limit_60, BGRimg1, 60, cropX, cropY)
 
     cv.imshow('cropGRAYimg1', cropGRAYimg1)
     cv.imshow('RecognitionRGB', BGRimg1)
     k = cv.waitKey(5) & 0xFF
     if k == 27:
         break
-
+    #cv.waitKey(1000)
 cv.destroyAllWindows()
